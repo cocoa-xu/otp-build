279692f5a980caffcc42e7cb8497f35fd99388ef
Cocoa <i@uwucocoa.moe>
2023-12-25 01:24:31 +0800

    try fixing ci

--- .github/do-build-manylinux2014.sh
+++ .github/do-build-manylinux2014.sh
@@ -9,7 +9,11 @@ OPENSSL_VERSION=${OPENSSL_VERSION:-3.2.0}
 PERFIX_DIR="/openssl-${ARCH}-linux-gnu"
 OPENSSL_ARCHIVE="openssl-${ARCH}-linux-gnu.tar.gz"
 
-yum install -y ncurses-devel && \
+if [ -d "/usr/lib64" ]; then
+    sudo ln -s /usr/lib64 /lib64
+fi
+
+yum install -y ncurses-devel curl && \
     cd / && \
     curl -fSL "https://github.com/cocoa-xu/openssl-build/releases/download/v${OPENSSL_VERSION}/${OPENSSL_ARCHIVE}" -o "${OPENSSL_ARCHIVE}" && \
     tar -C / -xf "${OPENSSL_ARCHIVE}" && \

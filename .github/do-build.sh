d7d68c64dafbbbe79fc41af77bc86ce8b05a2ea8
Cocoa <i@uwucocoa.moe>
2024-01-08 19:27:52 +0700

    fix build script

---
+++ .github/do-build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -x
+
+GITHUB_REF_NAME=$1
+ARCH=$2
+TRIPLET=$3
+
+OTP_VERSION=${GITHUB_REF_NAME##v}
+OPENSSL_VERSION=${OPENSSL_VERSION:-3.2.0}
+OPENSSL_PERFIX_DIR="/work/openssl-${OPENSSL_VERSION}-${TRIPLET}"
+OPENSSL_ARCHIVE="openssl-${OPENSSL_VERSION}-${TRIPLET}.tar.gz"
+
+case $TRIPLET in
+    *-linux-musl)
+        apk add gcc g++ perl ncurses-dev
+        ;;
+    *-linux-gnu)
+        ;;
+    *)
+        echo "Unknown TRIPLET: ${TRIPLET}"
+        exit 1
+        ;;
+esac
+
+cd /work && \
+    mkdir -p "${OPENSSL_PERFIX_DIR}" && \
+    tar -C "${OPENSSL_PERFIX_DIR}" -xf "${OPENSSL_ARCHIVE}" && \
+    tar -xf "otp_src_${GITHUB_REF_NAME}.tar.gz" && \
+    cd "otp_src_${OTP_VERSION}" && \
+    ./configure --without-javac --with-ssl="${OPENSSL_PERFIX_DIR}" --disable-dynamic-ssl-lib && \
+    make -j"$(nproc)" && \
+    make DESTDIR="$(pwd)/otp_${OTP_VERSION}" install && \
+    cd "otp_${OTP_VERSION}" && \
+    tar -czf "/work/otp-${TRIPLET}.tar.gz" .
--- .github/script-to-build-manylinux2014.sh
+++ .github/script-to-build-manylinux2014.sh
@@ -6,4 +6,4 @@ OTP_VERSION=$1
 ARCH=$2
 
 sudo docker run --privileged --network=host --rm -v `pwd`:/work "quay.io/pypa/manylinux2014_$ARCH:latest" \
-    sh -c "chmod a+x /work/do-build-manylinux2014.sh && /work/do-build-manylinux2014.sh ${OTP_VERSION} ${ARCH}"
+    sh -c "chmod a+x /work/do-build.sh && /work/do-build.sh ${OTP_VERSION} ${ARCH} ${ARCH}-linux-gnu"

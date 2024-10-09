97f25fc49908d72e9e171e9bc5e6760132482b96
Cocoa <i@uwucocoa.moe>
2024-10-10 00:13:54 +0100

    armv7l-linux-guneabihf
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/do-build.sh
+++ .github/do-build.sh
@@ -15,6 +15,9 @@ case $TRIPLET in
     riscv64-linux-gnu )
         apt-get update && apt-get install -y gcc g++ perl libncurses5-dev make
         ;;
+    armv7l-linux-gnueabihf )
+        apt-get update && apt-get install -y libncurses-dev libssl-dev make cmake build-essential gcc g++ curl
+        ;;
     *-linux-gnu )
         yum install -y gcc gcc-c++ perl ncurses-devel make
         ;;
@@ -28,7 +31,7 @@ case $TRIPLET in
 esac
 
 case $TRIPLET in
-    i686-* | i386-* )
+    i686-* | i386-* | armv7l-* )
         CONFIGURE_OPTIONS="--disable-year2038"
         ;;
     * )
@@ -36,14 +39,22 @@ case $TRIPLET in
         ;;
 esac
 
-cd /work && \
+cd /work
+
+if [ "${OPENSSL_VERSION}" != "nil" ]; then
     mkdir -p "${OPENSSL_PERFIX_DIR}" && \
-    tar -C "${OPENSSL_PERFIX_DIR}" -xzf "${OPENSSL_ARCHIVE}" && \
-    rm -rf "otp_src_${OTP_VERSION}" && \
+    tar -C "${OPENSSL_PERFIX_DIR}" -xzf "${OPENSSL_ARCHIVE}" ;
+fi
+
+rm -rf "otp_src_${OTP_VERSION}" && \
     mkdir -p "otp_src_${OTP_VERSION}" && \
     tar -xzf "otp_src_${GITHUB_REF_NAME}.tar.gz" -C "otp_src_${OTP_VERSION}" --strip-components=1 && \
     cd "otp_src_${OTP_VERSION}" && \
-    ./configure --without-javac --with-ssl="${OPENSSL_PERFIX_DIR}" --disable-dynamic-ssl-lib ${CONFIGURE_OPTIONS} && \
+    if [ "${OPENSSL_VERSION}" != "nil" ]; then
+        ./configure --without-javac --with-ssl="${OPENSSL_PERFIX_DIR}" --disable-dynamic-ssl-lib ${CONFIGURE_OPTIONS} ;
+    else
+        ./configure --without-javac --disable-dynamic-ssl-lib ${CONFIGURE_OPTIONS} ;
+    fi && \
     make -j"$(nproc)" && \
     make DESTDIR="$(pwd)/otp_${OTP_VERSION}" install && \
     cd "otp_${OTP_VERSION}" && \

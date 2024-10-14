7724f1eac44d2013e3645ce4e723ca93944ee000
Cocoa <i@uwucocoa.moe>
2024-10-15 00:54:52 +0100

    fix deps
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/install-deps-linux-glibc.sh
+++ .github/install-deps-linux-glibc.sh
@@ -11,4 +11,6 @@ else
 fi
 
 ${SUDO} apt-get update
-${SUDO} apt-get install -y gcc g++ perl libncurses5-dev make build-essential curl
+${SUDO} apt-get install -y gcc g++ perl libncurses5-dev make build-essential curl \
+  libgtk-3-dev libwebkit2gtk-4.0-dev liblzma-dev libcurl4-openssl-dev libnotify-dev \
+  libsecret-1-dev libgspell-1-dev libgl1-mesa-dev libglu1-mesa-dev
